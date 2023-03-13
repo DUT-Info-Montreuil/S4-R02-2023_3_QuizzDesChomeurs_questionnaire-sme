234f400e5302a879812ebe5ba23dce83e5c4c636
Xiang Luc <xiangluc01@gmail.com>
2023-03-13 19:50:38 +0100

    ajout des méthodes à implanter dans IServiceQuestionnaire : fournirListeQuestionnaires et fournirUnQuestionnaire

--- src/fr/iutmontreuil/S04_R02_2023_3_QuizzDesChomeurs_Questionnaire_sme/entities/modeles/IServiceQuestionnaire.java
+++ src/fr/iutmontreuil/S04_R02_2023_3_QuizzDesChomeurs_Questionnaire_sme/entities/modeles/IServiceQuestionnaire.java
@@ -1,9 +1,10 @@
 package fr.iutmontreuil.S04_R02_2023_3_QuizzDesChomeurs_Questionnaire_sme.entities.modeles;
 
-import fr.iutmontreuil.S04_R02_2023_3_QuizzDesChomeurs_Questionnaire_sme_entities.dto.QuestionDTO;
+import fr.iutmontreuil.S04_R02_2023_3_QuizzDesChomeurs_Questionnaire_sme_entities.dto.QuestionnairesDTO;
 
 import java.util.ArrayList;
 
 public interface IServiceQuestionnaire {
-    public ArrayList<QuestionDTO> fournirListeQuestions();
+    public ArrayList<QuestionnairesDTO> fournirListeQuestionnaires(QuestionnairesDTO questionnaire);
+    public QuestionnairesDTO fournirUnQuestionnaire(String fichierCSV);
 }
