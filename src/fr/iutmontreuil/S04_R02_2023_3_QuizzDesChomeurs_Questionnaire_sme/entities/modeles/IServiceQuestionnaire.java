122888080123aa70ea07c0fa9f38db54541aeab6
Xiang Luc <xiangluc01@gmail.com>
2023-03-16 15:56:47 +0100

    Mise à jour de l'interface

--- src/fr/iutmontreuil/S04_R02_2023_3_QuizzDesChomeurs_Questionnaire_sme/entities/modeles/IServiceQuestionnaire.java
+++ src/fr/iutmontreuil/S04_R02_2023_3_QuizzDesChomeurs_Questionnaire_sme/entities/modeles/IServiceQuestionnaire.java
@@ -1,10 +1,13 @@
 package fr.iutmontreuil.S04_R02_2023_3_QuizzDesChomeurs_Questionnaire_sme.entities.modeles;
 
+import fr.iutmontreuil.S04_R02_2023_3_QuizzDesChomeurs_Questionnaire_sme_entities.dto.QuestionDTO;
 import fr.iutmontreuil.S04_R02_2023_3_QuizzDesChomeurs_Questionnaire_sme_entities.dto.QuestionnairesDTO;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public interface IServiceQuestionnaire {
-    public ArrayList<QuestionnairesDTO> fournirListeQuestionnaires(QuestionnairesDTO questionnaire);
-    public QuestionnairesDTO fournirUnQuestionnaire(String fichierCSV);
+
+    public ArrayList<QuestionnairesDTO> fournirListeQuestionnaires();
+    public List<QuestionDTO> fournirUnQuestionnaire(String fichierCSV);
 }
